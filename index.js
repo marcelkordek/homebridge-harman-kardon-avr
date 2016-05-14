52568521a73e2591e53411fe349f484d826034df
Marcel Kordek <marcelkordek@me.com>
2016-05-15 00:46:54 +0200

    update

--- index.js
+++ index.js
@@ -60,11 +60,7 @@ HarmanKardonAVRAccessory.Input = function () {
 HarmanKardonAVRAccessory.Mute = function () {
     Characteristic.call(this, 'Mute', '6b5e0bed-fdbe-40b6-84e1-12ca1562babd');
     this.setProps({
-    format: Characteristic.Formats.INT,
-    unit: Characteristic.Units.PERCENTAGE,
-    maxValue: 100,
-    minValue: 0,
-    minStep: 1,
+    format: Characteristic.Formats.BOOL,
     perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE, Characteristic.Perms.NOTIFY]
   });
     this.value = this.getDefaultValue();
